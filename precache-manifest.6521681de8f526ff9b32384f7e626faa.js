7b2eaa11838fca232d0c9c7c3eaa384a85cffecb
ThayalanGR <grthayalan18@gmail.com>
2019-04-27 21:19:14 +0530

    Updates

---
+++ precache-manifest.6521681de8f526ff9b32384f7e626faa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32f98bb5bc8b2a06672e8e70014c442",
+    "url": "/kurinji-college-website/index.html"
+  },
+  {
+    "revision": "42181accf8836a7d2e47",
+    "url": "/kurinji-college-website/static/css/2.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "38736545b2d1e6d5a83e",
+    "url": "/kurinji-college-website/static/css/main.5b1790f7.chunk.css"
+  },
+  {
+    "revision": "42181accf8836a7d2e47",
+    "url": "/kurinji-college-website/static/js/2.b265422f.chunk.js"
+  },
+  {
+    "revision": "38736545b2d1e6d5a83e",
+    "url": "/kurinji-college-website/static/js/main.480a645f.chunk.js"
+  },
+  {
+    "revision": "a0d4848aecff82cb4433",
+    "url": "/kurinji-college-website/static/js/runtime~main.a55c290c.js"
+  },
+  {
+    "revision": "76937f67a02b8116f55512eff5c39014",
+    "url": "/kurinji-college-website/static/media/kurinji.76937f67.png"
+  }
+]);
