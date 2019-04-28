9411977097b5f769081be9af382084e678e61792
ThayalanGR <grthayalan18@gmail.com>
2019-04-29 01:43:56 +0530

    new backend wired up and landing page tweaking kdone

--- src/src/components/FirstSection.jsx
+++ src/src/components/FirstSection.jsx
@@ -2,151 +2,270 @@ import React, { Component } from 'react'
 import { toast } from 'react-toastify';
 import kurinji from '../images/kurinji.png';
 import axios from 'axios';
+import '../css/toastmod.css'
 
 const baseUrl = 'http://localhost/kurinji';
 
 export default class FirstSection extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          carouselDataStore: [],
-          carouselData: {
-            imageUrl: '',
-            information: ''
-          },
-          news: []
-       }
-       this.fetchEvents = this.fetchEvents.bind(this);
-       this.initiateCarousel = this.initiateCarousel.bind(this);
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      carouselDataStore: [],
+      carouselData: {
+        imageUrl: '',
+        information: ''
+      },
+      news: [],
+      newsText: ''
     }
+    this.fetchEvents = this.fetchEvents.bind(this);
+    this.initiateCarousel = this.initiateCarousel.bind(this);
+    this.initiateNews = this.initiateNews.bind(this);
+    this.showNotification = this.showNotification.bind(this);
+  }
 
-    componentWillMount() {
-      this.fetchEvents();
+  componentWillMount() {
+
+    this.fetchEvents();
+    this.fetchNews();
+
+  }
+
+  fetchEvents() {
+    axios
+      .get("http://localhost/kurinji/api/homesectionone.php")
+      .then(data => {
+        this.setState({ carouselDataStore: data.data })
+        this.initiateCarousel()
+
+      })
+      .catch(err => console.log(err))
+  }
+
+  fetchNews() {
+    axios
+      .get("http://localhost/kurinji/api/homenews.php")
+      .then(data => {
+        this.setState({ news: data.data })
+        this.initiateNews()
+      })
+      .catch(err => console.log(err))
+  }
+
+  showNotification() {
+    if (window.pageYOffset === 0) {
+      toast.dismiss();
+      toast(<div className="container-fluid">
+        <div className="row">
+          <div className="col text-center font-weight-bold h5 mt-1">
+            courses offered
+                <hr className="white text-white" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col text-center font-weight-bolder h5">
+            UG - Courses
+                  <hr className="text-white white" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">MECH</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">EEE</span>
+          </div>
+        </div>
+        <div className="row mt-2 pt-4">
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">ECE</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">CSE</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">IT</span>
+          </div>
+        </div>
+
+        <div className="row mt-2 pt-3">
+          <div className="col text-center font-weight-bold h5">
+            PG - Courses
+                  <hr className="text-white white" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col text-center font-weight-bolder">
+            <span className="bg-warning shadow-lg p-2 text-white rounded m-2">ME - CSE</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="bg-warning shadow-lg p-2 text-white rounded m-2">Eng Design</span>
+          </div>
+        </div>
+        <div className="row mt-3 pt-3 mb-3">
+          <div className="col text-center font-weight-bolder">
+            <span className="bg-warning shadow-lg p-2 text-white rounded m-2">MBA</span>
+          </div>
+        </div>
+      </div>, {
+          position: "bottom-right",
+          className: "bg-danger text-white Toastify__toast1",
+          autoClose: false
+        });
+    } else {
+
+      toast.dismiss();
 
     }
+  }
 
-    fetchEvents() {
-      axios
-          .get("http://localhost/kurinji/api/homesectionone.php")
-          .then(data => {
-                this.setState({ carouselDataStore: data.data })
-                this.initiateCarousel()
+  componentWillUnmount() {
+    toast.dismiss();
+  }
 
-          })
-          .catch(err => console.log(err))
-    }
-    
-    componentDidMount() {
+  componentDidMount() {
+    window.onscroll = function () {
+      this.showNotification();
+    }.bind(this)
 
-        setTimeout(() => {
-          toast.dismiss();
-          toast(<div className="container-fluid">
-              <div className="row">
-                <div className="col text-center font-weight-bold">
-                  courses offered
+
+    setTimeout(() => {
+      toast.dismiss();
+      toast(<div className="container-fluid">
+        <div className="row">
+          <div className="col text-center font-weight-bold h5 mt-1">
+            courses offered
                   <hr className="white text-white" />
-                </div>
-              </div>
-          
-              <div className="row">
-                <div className="col text-center font-weight-bolder">
-                    UG - Courses
-                    <hr className="text-white white"/>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col text-center font-weight-bolder">
-                    <span className="white shadow-lg p-2 text-danger rounded m-2">MECH</span>  
-                </div>
-                <div className="col text-center font-weight-bolder">
-                <span className="white shadow-lg p-2 text-danger rounded m-2">EEE</span>  
-                </div>
-              </div>
-              <div className="row mt-2 pt-4">
-                <div className="col text-center font-weight-bolder">
-                <span className="white shadow-lg p-2 text-danger rounded m-2">ECE</span>  
-                </div>
-                <div className="col text-center font-weight-bolder">
-                <span className="white shadow-lg p-2 text-danger rounded m-2">CSE</span>  
-                </div>
-                <div className="col text-center font-weight-bolder">
-                <span className="white shadow-lg p-2 text-danger rounded m-2">IT</span>  
-                </div>
-              </div>
-    
-              <div className="row mt-2 pt-3">
-                <div className="col text-center font-weight-bolder">
-                    UG - Courses
-                    <hr className="text-white white"/>
-                </div>
-              </div>
-    
-              <div className="row">
-                <div className="col text-center font-weight-bolder">
-                    <span className="white shadow-lg p-2 text-danger rounded m-2">ME - CSE</span>  
-                </div>
-                <div className="col text-center font-weight-bolder">
-                <span className="white shadow-lg p-2 text-danger rounded m-2">Eng Design</span>  
-                </div>
-              </div>
-              <div className="row mt-3 pt-3">
-                <div className="col text-center font-weight-bolder">
-                    <span className="white shadow-lg p-2 text-danger rounded m-2">MBA</span>  
-                </div>
-              </div>
-          </div>, {
-            position: toast.POSITION.BOTTOM_LEFT,
-            className: "bg-danger text-white",
-            autoClose: false
-          });
-        }, 3000)
-        
-    }
+          </div>
+        </div>
 
+        <div className="row">
+          <div className="col text-center font-weight-bold h5">
+            UG - Courses
+                    <hr className="text-white white" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">MECH</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">EEE</span>
+          </div>
+        </div>
+        <div className="row mt-2 pt-4">
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">ECE</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">CSE</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="red shadow-lg p-2 text-white rounded m-2">IT</span>
+          </div>
+        </div>
 
-    initiateCarousel() {
-        let i = 0;
-        this.setState({
-          carouselData: {
-            imageUrl: `${baseUrl}${this.state.carouselDataStore[i][2]}`,
-            information: this.state.carouselDataStore[i][1]
-          }
-        })
-        setInterval(() => {
-          if (i < this.state.carouselDataStore.length - 1) {
-            i++;
-          } else {
-            i = 0;
-          }
-          this.setState({
-            carouselData: {
-              imageUrl: `${baseUrl}${this.state.carouselDataStore[i][2]}`,
-              information: this.state.carouselDataStore[i][1]
-            }
-          })
-        }, 5000)
-     }
+        <div className="row mt-2 pt-3">
+          <div className="col text-center font-weight-bold h5">
+            PG - Courses
+                    <hr className="text-white white" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col text-center font-weight-bolder">
+            <span className="bg-warning shadow-lg p-2 text-white rounded m-2">ME - CSE</span>
+          </div>
+          <div className="col text-center font-weight-bolder">
+            <span className="bg-warning shadow-lg p-2 text-white rounded m-2">Eng Design</span>
+          </div>
+        </div>
+        <div className="row mt-3 pt-3 mb-3">
+          <div className="col text-center font-weight-bolder">
+            <span className="bg-warning shadow-lg p-2 text-white rounded m-2">MBA</span>
+          </div>
+        </div>
+      </div>, {
+          position: "bottom-right",
+          className: "bg-danger text-white Toastify__toast1",
+          autoClose: false
+        });
+    }, 3000)
+
+  }
+
+  initiateCarousel() {
+    let i = 0;
+    this.setState({
+      carouselData: {
+        imageUrl: `${baseUrl}${this.state.carouselDataStore[i][2]}`,
+        information: this.state.carouselDataStore[i][1]
+      }
+    })
+    setInterval(() => {
+      if (i < this.state.carouselDataStore.length - 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+      this.setState({
+        carouselData: {
+          imageUrl: `${baseUrl}${this.state.carouselDataStore[i][2]}`,
+          information: this.state.carouselDataStore[i][1]
+        }
+      })
+    }, 5000)
+  }
+
+  initiateNews() {
+    let newsString = '';
+    // eslint-disable-next-line
+    this.state.news.map((item, i) => {
+      if (i === 0) {
+        newsString += `${item[1]} `;
+      }
+      newsString += `|| ${item[1]} `;
+    })
+    this.setState({ newsText: newsString })
+  }
 
   render() {
     return (
       <div className="row background-image" style={{ backgroundImage: `url(${this.state.carouselData.imageUrl})` }}>
-        <div className="col d-flex justify-content-between flex-column align-items-center
-        " style={{marginTop: "30vh"}}>
-
+        <div className="col d-flex justify-content-between flex-column align-items-center"
+          style={{ marginTop: "30vh" }}>
           <div className="align-self-start">
-            <div className="text-white bg-danger alert bg-danger" style={{fontSize: "20px"}}>
-              <div className="text-center animated shake">
-                {this.state.carouselData.information}
+            <div className="mask pattern-3 rounded">
+              <div className="text-white alert" style={{ fontSize: "20px" }}>
+                <div className="text-center animated shake">
+                  {this.state.carouselData.information}
+                </div>
               </div>
             </div>
           </div>
-          <div className="alert text-danger align-self-end white mb-0" role="alert">
-                <p className="font-weight-bold animated flash infinite slower delay-3s"><i className="fas fa-star "></i> Latest News </p> 
-                {/* eslint-disable-next-line  */}
-                <marquee><p style={{fontFamily: "Impact", fontSize: "16pt"}}>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor!</p></marquee>
+
+          <div className="alert text-danger align-self-end  mb-0 background-transparent p-0 m-0" role="alert">
+
+            <div className="mask rgba-white-strong pattern-5">
+              <p className="font-weight-bold animated flash infinite text-warning slower delay-3s mt-3 pt-3 pl-3 h5">
+                <i className="fas fa-star mr-2 animated heartBeat infinite"></i>
+                Latest News
+                 </p>
+              {/* eslint-disable-next-line  */}
+              <marquee><p className="text-white" style={{ fontFamily: "Impact", fontSize: "15pt" }}>
+                {this.state.newsText}
+              </p></marquee>
+            </div>
+
           </div>
 
           <div className="bg-mask p-0 m-0"></div>
+
+
+
           <div className="row header-row d-flex justify-content-center align-items-center">
             <div className="col-md-6 col-sm-12">
               <img className="header-image mt-1 img-fluid" src={kurinji} alt="" srcSet="" />
@@ -156,7 +275,7 @@ export default class FirstSection extends Component {
                 <div>
                   <i className="fas fa-map-marker-alt text-danger fa-2x pr-3 mb-4"></i>
                 </div>
-                <p className="text-center  font-weight-bold h6" style={{ }}>
+                <p className="text-center text-white font-weight-bold h6" style={{}}>
                   Trichy-Dindigul NH-Road,
                 Manapparai - 621 307 <br />
                   Trichirappalli Dt.
@@ -167,7 +286,7 @@ export default class FirstSection extends Component {
                 <div>
                   <i className="fas fa-phone text-danger fa-2x pr-3 mb-4"></i>
                 </div>
-                <p className="text-center h6 font-weight-bold" style={{  }}>
+                <p className="text-center h6 font-weight-bold text-white" style={{}}>
                   04332&nbsp;292338
                   76038&nbsp;44448
               </p>
@@ -176,7 +295,7 @@ export default class FirstSection extends Component {
                 <div>
                   <i className="far fa-envelope text-danger fa-2x pr-3 mb-4"></i>
                 </div>
-                <p className="font-weight-bold" style={{ }}>
+                <p className="font-weight-bold text-white" style={{}}>
                   kcet@kurinjiengg.org
                   &nbsp;
                   principal@kurinjiengg.org
@@ -184,9 +303,10 @@ export default class FirstSection extends Component {
               </div>
             </div>
           </div>
-    
+
         </div>
       </div>
     )
   }
+  
 }
