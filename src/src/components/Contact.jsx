76c3f2fcd31f21e768f40178e6e6c73eeb039186
ThayalanGR <grthayalan18@gmail.com>
2019-05-20 10:55:26 +0530

    contact us page completed

--- src/src/components/Contact.jsx
+++ src/src/components/Contact.jsx
@@ -1,5 +1,7 @@
-import React, { Component, Fragment } from 'react'
-import {Loader, } from '../components' 
+import React, { Component } from 'react'
+import { Loader, } from '../components'
+import '../css/contactus.css'
+import kurinji from '../images/kurinji.png'
 
 export default class Contact extends Component {
   constructor(props) {
@@ -19,14 +21,24 @@ export default class Contact extends Component {
 
   render() {
     return (
-      this.state.isLoading ? <Loader type={"bars"} /> : <Fragment>
-        
-      <div className="mtspace">
-        
-        
-        contact us</div>
-      </Fragment>
+      this.state.isLoading ? <Loader type={"bars"} /> :
+        <div className="mtspace contactus">
+          <div class="container">
+            <div class="card mb-3 box-img-contact-us shadow-none">
+              <img class="rounded img-responsive" src={kurinji} alt="Dot" />
+              <div class="box-contact-us">
+                <div class="card-title text-center title orange-text mt-1 h4-responsive">CONTACT DETAILS</div>
+                <ul class="list-group list-group-flush ul-contact-us">
+                  <li class="list-group-item text-danger p-4"><b class="text-primary h4-responsive">Telephone Number : </b> <br /> 04332 - 292338 / 76038 44448</li>
+                  <li class="list-group-item text-danger p-4"><b class="text-primary h4-responsive"> E-mail : </b> <br /> kcet@kurinjiengg.org / principal@kurinjiengg.org</li>
+                  <li class="list-group-item text-danger p-4"><b class="text-primary h4-responsive"> Web : </b> <br /> www.kurinjiengg.org</li>
+                  <li class="list-group-item text-danger p-4"><b class="text-primary h4-responsive"> Address : </b> <br /> Trichy-Dindigul NH-Road,<br />Manapparai - 621 307<br />Trichirappalli Dt.<br />Tamilnadu, India.</li>
+                </ul>
+              </div>
+            </div>
 
+          </div>
+        </div>
     )
   }
 }
