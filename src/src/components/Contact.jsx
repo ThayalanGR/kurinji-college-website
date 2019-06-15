6b677e5982b38cb7d465084b004820ad90fb0e0e
Ambarish PK <aambu1998@gmail.com>
2019-06-15 18:37:59 +0530

    contactus module implemented

--- src/src/components/Contact.jsx
+++ src/src/components/Contact.jsx
@@ -1,44 +1,101 @@
-import React, { Component } from 'react'
-import { Loader, } from '../components'
-import '../css/contactus.css'
-import kurinji from '../images/kurinji.png'
+import React, { Component } from "react";
+import { Loader } from "../components";
+import "../css/contactus.css";
+import kurinji from "../images/kurinji.png";
 
 export default class Contact extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isLoading: true
-    }
+    };
   }
   componentDidMount() {
-
     setTimeout(() => {
       this.setState({ isLoading: false });
-    }, 500)
-
+    }, 500);
   }
 
-
   render() {
-    return (
-      this.state.isLoading ? <Loader type={"bars"} /> :
-        <div className="mtspace contactus">
-          <div className="container">
-            <div className="card mb-3 box-img-contact-us shadow-none">
-              <img className="rounded img-responsive" src={kurinji} alt="Dot" />
-              <div className="box-contact-us">
-                <div className="card-title text-center title orange-text mt-1 h4-responsive">CONTACT DETAILS</div>
-                <ul className="list-group list-group-flush ul-contact-us">
-                  <li className="list-group-item text-danger p-4"><b className="text-primary h4-responsive">Telephone Number : </b> <br /> 04332 - 292338 / 76038 44448</li>
-                  <li className="list-group-item text-danger p-4"><b className="text-primary h4-responsive"> E-mail : </b> <br /> kcet@kurinjiengg.org / principal@kurinjiengg.org</li>
-                  <li className="list-group-item text-danger p-4"><b className="text-primary h4-responsive"> Web : </b> <br /> www.kurinjiengg.org</li>
-                  <li className="list-group-item text-danger p-4"><b className="text-primary h4-responsive"> Address : </b> <br /> Trichy-Dindigul NH-Road,<br />Manapparai - 621 307<br />Trichirappalli Dt.<br />Tamilnadu, India.</li>
-                </ul>
+    return this.state.isLoading ? (
+      <Loader type={"bars"} />
+    ) : (
+      <div className="mtspace contactus">
+        <div className="container">
+          <div className="card mb-3 box-img-contact-us shadow-none">
+            <img className="rounded img-responsive" src={kurinji} alt="Dot" />
+            <div className="text-primary mt-3 h4-responsive text-center">
+              Contact Us
+            </div>
+            <hr />
+            <div className="d-flex justify-content-around align-items-center col-md-12 text-center mt-5">
+              <div class="card-deck">
+                <div class="card p-3 hoverable contactbox">
+                  <i class="fa fa-phone fa-8x text-primary" aria-hidden="true" />
+                  <div class="card-body">
+                    <h5 class="card-title text-danger font-weight-bold">Telephone</h5>
+                    <p class="card-text text-danger">
+                      04332 - 292338 / <br /> 76038 44448
+                    </p>
+                  </div>
+                </div>
+                <div class="card p-3 hoverable contactbox">
+                  <i class="fa fa-envelope fa-8x text-primary" aria-hidden="true" />
+                  <div class="card-body">
+                    <h5 class="card-title text-danger font-weight-bold">Email</h5>
+                    <p class="card-text text-danger">
+                      kcet@kurinjiengg.org / principal@kurinjiengg.org
+                    </p>
+                  </div>
+                </div>
+                <div class="card p-3 hoverable contactbox">
+                  <i class="fa fa-link fa-8x text-primary" aria-hidden="true" />
+                  <div class="card-body">
+                    <h5 class="card-title text-danger font-weight-bold">Website</h5>
+                    <p class="card-text text-danger">www.kurinjiengg.org</p>
+                  </div>
+                </div>
+                <div class="card p-3 hoverable contactbox">
+                  <i class="fa fa-address-card fa-8x text-primary" aria-hidden="true" />
+                  <div class="card-body">
+                    <h5 class="card-title text-danger font-weight-bold">Address</h5>
+                    <p class="card-text text-danger">
+                      Trichy-Dindigul NH-Road,
+                      <br />
+                      Manapparai - 621 307, Trichirappalli Dt.
+                      <br />
+                      Tamilnadu, India.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-
+            {/* <div className="d-flex justify-content-around align-items-center mt-1 col-md-12 col-sm-12 text-center">
+              <div className="card-deck">
+                <div class="card p-3">
+                  <i class="fa fa-link fa-5x" aria-hidden="true" />
+                  <div class="card-body">
+                    <h5 class="card-title">Website</h5>
+                    <p class="card-text">www.kurinjiegg.org</p>
+                  </div>
+                </div>
+                <div class="card p-3">
+                  <i class="fa fa-map-marker fa-5x" aria-hidden="true" />
+                  <div class="card-body">
+                    <h5 class="card-title">Address</h5>
+                    <p class="card-text">
+                      Trichy-Dindigul NH-Road,<br/>
+                      Manapparai - 621 307,
+                      Trichirappalli Dt.<br/>
+                      Tamilnadu, India.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div> */}
           </div>
         </div>
-    )
+      </div>
+    );
   }
 }
