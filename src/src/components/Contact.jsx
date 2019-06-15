310b5627f6a6b1e7d4f173e6750584e94a857170
Ambarish PK <aambu1998@gmail.com>
2019-06-15 20:03:25 +0530

    contactus component bugs cleared

--- src/src/components/Contact.jsx
+++ src/src/components/Contact.jsx
@@ -22,77 +22,72 @@ export default class Contact extends Component {
     ) : (
       <div className="mtspace contactus">
         <div className="container">
-          <div className="card mb-3 box-img-contact-us shadow-none">
-            <img className="rounded img-responsive" src={kurinji} alt="Dot" />
-            <div className="text-primary mt-3 h4-responsive text-center">
+          <div className="row">
+            <div className="col text-center">
+              <img className="rounded img-responsive" src={kurinji} alt="Dot" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col text-primary font-weight-bold mt-3 h3-responsive text-center">
               Contact Us
+              <hr />
             </div>
-            <hr />
-            <div className="d-flex justify-content-around align-items-center col-md-12 text-center mt-5">
-              <div class="card-deck">
-                <div class="card p-3 hoverable contactbox">
-                  <i class="fa fa-phone fa-8x text-primary" aria-hidden="true" />
-                  <div class="card-body">
-                    <h5 class="card-title text-danger font-weight-bold">Telephone</h5>
-                    <p class="card-text text-danger">
-                      04332 - 292338 / <br /> 76038 44448
-                    </p>
-                  </div>
-                </div>
-                <div class="card p-3 hoverable contactbox">
-                  <i class="fa fa-envelope fa-8x text-primary" aria-hidden="true" />
-                  <div class="card-body">
-                    <h5 class="card-title text-danger font-weight-bold">Email</h5>
-                    <p class="card-text text-danger">
-                      kcet@kurinjiengg.org / principal@kurinjiengg.org
-                    </p>
-                  </div>
-                </div>
-                <div class="card p-3 hoverable contactbox">
-                  <i class="fa fa-link fa-8x text-primary" aria-hidden="true" />
-                  <div class="card-body">
-                    <h5 class="card-title text-danger font-weight-bold">Website</h5>
-                    <p class="card-text text-danger">www.kurinjiengg.org</p>
-                  </div>
-                </div>
-                <div class="card p-3 hoverable contactbox">
-                  <i class="fa fa-address-card fa-8x text-primary" aria-hidden="true" />
-                  <div class="card-body">
-                    <h5 class="card-title text-danger font-weight-bold">Address</h5>
-                    <p class="card-text text-danger">
-                      Trichy-Dindigul NH-Road,
-                      <br />
-                      Manapparai - 621 307, Trichirappalli Dt.
-                      <br />
-                      Tamilnadu, India.
-                    </p>
-                  </div>
+          </div>
+          <div className="row">
+            <div className="col-md-3 col-sm-12">
+              <div class="hoverable box-contact-us">
+                <i class="fa fa-phone fa-4x text-primary" aria-hidden="true" />
+                <div class="">
+                  <h5 class="text-danger font-weight-bold">Telephone</h5>
+                  <p class="text-danger">
+                    04332 - 292338 / <br /> 76038 44448
+                  </p>
                 </div>
               </div>
             </div>
-            {/* <div className="d-flex justify-content-around align-items-center mt-1 col-md-12 col-sm-12 text-center">
-              <div className="card-deck">
-                <div class="card p-3">
-                  <i class="fa fa-link fa-5x" aria-hidden="true" />
-                  <div class="card-body">
-                    <h5 class="card-title">Website</h5>
-                    <p class="card-text">www.kurinjiegg.org</p>
-                  </div>
-                </div>
-                <div class="card p-3">
-                  <i class="fa fa-map-marker fa-5x" aria-hidden="true" />
-                  <div class="card-body">
-                    <h5 class="card-title">Address</h5>
-                    <p class="card-text">
-                      Trichy-Dindigul NH-Road,<br/>
-                      Manapparai - 621 307,
-                      Trichirappalli Dt.<br/>
-                      Tamilnadu, India.
-                    </p>
-                  </div>
+            <div className="col-md-3 col-sm-12">
+              <div class="hoverable box-contact-us">
+                <i
+                  class="fa fa-envelope fa-4x text-primary"
+                  aria-hidden="true"
+                />
+                <div class="">
+                  <h5 class="text-danger font-weight-bold">Email</h5>
+                  <p class="text-danger">
+                    kcet@kurinjiengg.org / principal@kurinjiengg.org
+                  </p>
                 </div>
               </div>
-            </div> */}
+            </div>
+            <div className="col-md-3 col-sm-12">
+              <div class="hoverable box-contact-us">
+                <i class="fa fa-link fa-4x text-primary" aria-hidden="true" />
+                <div class="">
+                  <h5 class="text-danger font-weight-bold">Website</h5>
+                  <p class="text-danger">www.kurinjiengg.org</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3 col-sm-12">
+              <div class="hoverable box-contact-us">
+                <i
+                  class="fa fa-address-card fa-4x text-primary"
+                  aria-hidden="true"
+                />
+                <div class="">
+                  <h5 class="text-danger font-weight-bold">Address</h5>
+                  <p class="text-danger">
+                    Trichy-Dindigul NH-Road,
+                    <br />
+                    Manapparai - 621 307,
+                    <br />
+                    Trichirappalli Dt.
+                    <br />
+                    Tamilnadu, India.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
