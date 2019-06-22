ab3280444458a288c7fb796bf96127d1a8b98a3f
ThayalanGR <grthayalan18@gmail.com>
2019-06-22 19:07:56 +0530

    database backup for production added

--- database/db_kurinji.sql
+++ database/db_kurinji.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2019 at 09:11 AM
+-- Generation Time: Jun 22, 2019 at 03:35 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -25,121 +25,123 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_event`
+-- Table structure for table `tbl_stucorfiles`
 --
 
-CREATE TABLE `tbl_event` (
+CREATE TABLE `tbl_stucorfiles` (
   `id` int(11) NOT NULL,
-  `caption` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
   `filepath` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbl_event`
---
-
-INSERT INTO `tbl_event` (`id`, `caption`, `filepath`, `date`) VALUES
-(13, 'new caption fo new image', '/api/uploads/homesectionone/image.JPG', '2019-05-12 09:24:58'),
-(14, 'this is new image', '/api/uploads/homesectionone/JKS3923.JPG', '2019-05-12 09:25:07'),
-(15, 'new image', '/api/uploads/homesectionone/JKS3988.JPG', '2019-05-14 15:20:36'),
-(16, 'spacious atmosphere', '/api/uploads/homesectionone/JKS3922.JPG', '2019-05-17 05:42:36');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_news`
---
-
-CREATE TABLE `tbl_news` (
-  `id` int(11) NOT NULL,
-  `news` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbl_news`
---
-
-INSERT INTO `tbl_news` (`id`, `news`, `date`) VALUES
-(6, 'our students have won the 1st prize in international  boxing competition', '2019-04-28 16:43:25'),
-(7, 'Admissions for 2019 batch is going on for both UG and PG courses', '2019-04-28 18:48:32');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_staffs`
---
-
-CREATE TABLE `tbl_staffs` (
-  `sid` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `designation` varchar(255) NOT NULL,
-  `priority` int(11) NOT NULL,
+  `tag` varchar(255) NOT NULL,
   `department` varchar(255) NOT NULL,
-  `about` longtext NOT NULL
+  `staffid` int(11) NOT NULL,
+  `staffname` varchar(255) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_staffs`
+-- Dumping data for table `tbl_stucorfiles`
 --
 
-INSERT INTO `tbl_staffs` (`sid`, `name`, `designation`, `priority`, `department`, `about`) VALUES
-(4, 'Ambarish', 'Associate Professor', 1, 'eee', 'Ambarish have pursued UG under Anna university'),
-(6, 'John David', 'Assistant professor', 2, 'mech', 'he have completed pg under au'),
-(7, 'Ajithkumar', 'Assistant Professor', 2, 'mecse', 'Ajithkumar has completed UG under Bharathidhasan University'),
-(8, 'Vijay', 'Associate Professor', 1, 'ece', 'Vijay have pursued UG under Anna university'),
-(9, 'Vinoth', 'Head of the Department', 0, 'eee', 'Vinoth have pursued UG under Anna university'),
-(10, 'Raja', 'Others', 3, 'mba', 'Raja have pursued MBA under Anna university'),
-(11, 'Chandrasekar', 'Assistant Professor', 2, 'engdesign', 'Chandrasekar have pursued PG under Anna university'),
-(12, 'Varun', 'Head of Department', 0, 'cse', 'Works in ITC Infotech'),
-(13, 'Lavanya', 'student', 3, 'cse', 'dklasndkas'),
-(14, 'Vijay', 'hod', 2, 'cse', 'skjnskA'),
-(15, 'Thayalan', 'Student', 1, 'cse', 'lksjakjdalk');
+INSERT INTO `tbl_stucorfiles` (`id`, `filename`, `filepath`, `tag`, `department`, `staffid`, `staffname`, `semester`, `date`) VALUES
+(6, 'Mathematics', '/api/uploads/stucorfiles/bitmap20190524.zip', 'mech', 'mech', 8112323, 'Ambarish', 1, '2019-06-22 12:10:47'),
+(7, 'Javascript', '/api/uploads/stucorfiles/transcript.docx', 'Math', 'eee', 8112323, 'Ambarish', 2, '2019-06-22 12:12:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_stucorstaffs`
+--
+
+CREATE TABLE `tbl_stucorstaffs` (
+  `id` int(11) NOT NULL,
+  `staffid` int(11) NOT NULL,
+  `staffname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `forgotpassword` varchar(255) NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_stucorstaffs`
+--
+
+INSERT INTO `tbl_stucorstaffs` (`id`, `staffid`, `staffname`, `email`, `password`, `forgotpassword`, `mobile`, `department`, `active`) VALUES
+(4, 8112323, 'Ambarish', 'aambu1998@gmail.com', '25d55ad283aa400af464c76d713c07ad', 'e533e1cb453afd768666add28dcccbac', 2147483647, 'cse', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_stucorstudents`
+--
+
+CREATE TABLE `tbl_stucorstudents` (
+  `id` int(11) NOT NULL,
+  `studentid` int(11) NOT NULL,
+  `studentname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `forgotpassword` varchar(255) NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_stucorstudents`
+--
+
+INSERT INTO `tbl_stucorstudents` (`id`, `studentid`, `studentname`, `email`, `password`, `forgotpassword`, `mobile`, `department`, `active`) VALUES
+(9, 217217, 'Ambarish', 'aambu1998@gmail.com', 'ed2b1f468c5f915f3f1cf75d7068baae', '4c157189665b200f88a24bac64f441fc', 2147483647, 'cse', 1),
+(10, 217454, 'Thayalan G R', 'grthayalan18@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'f1abef445fe476e7bc02bc26a00434c1', 2147483647, 'hands', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tbl_event`
+-- Indexes for table `tbl_stucorfiles`
 --
-ALTER TABLE `tbl_event`
+ALTER TABLE `tbl_stucorfiles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_news`
+-- Indexes for table `tbl_stucorstaffs`
 --
-ALTER TABLE `tbl_news`
+ALTER TABLE `tbl_stucorstaffs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_staffs`
+-- Indexes for table `tbl_stucorstudents`
 --
-ALTER TABLE `tbl_staffs`
-  ADD PRIMARY KEY (`sid`);
+ALTER TABLE `tbl_stucorstudents`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tbl_event`
+-- AUTO_INCREMENT for table `tbl_stucorfiles`
 --
-ALTER TABLE `tbl_event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT for table `tbl_news`
---
-ALTER TABLE `tbl_news`
+ALTER TABLE `tbl_stucorfiles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `tbl_staffs`
+-- AUTO_INCREMENT for table `tbl_stucorstaffs`
 --
-ALTER TABLE `tbl_staffs`
-  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+ALTER TABLE `tbl_stucorstaffs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_stucorstudents`
+--
+ALTER TABLE `tbl_stucorstudents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
