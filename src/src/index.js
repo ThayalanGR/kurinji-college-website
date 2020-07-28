bb4b154f0f61da82e96f1f3806e2fc3be4901226
ThayalanGR <grthayalan18@gmail.com>
2020-07-28 09:29:01 +0530

    sticky top for navbar added

--- src/src/index.js
+++ src/src/index.js
@@ -22,31 +22,33 @@ toast.configure();
 
 ReactDOM.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div className="core-container">
+        <>
             <Navbar />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/infrastructure" component={Infrastructure} />
-                <Route exact path="/gallery" component={Gallery} />
-                <Route exact path="/alumni" component={Alumni} />
-                <Route exact path="/contact" component={Contact} />
-                <Route exact path="/mech" component={Mech} />
-                <Route exact path="/eee" component={Eee} />
-                <Route exact path="/ece" component={Ece} />
-                <Route exact path="/cse" component={Cse} />
-                <Route exact path="/hands" component={Hands} />
-                <Route exact path="/mecse" component={Mecse} />
-                <Route exact path="/engdesign" component={Engdesign} />
-                <Route exact path="/mba" component={Mba} />
-                <Route exact path="/historyofcollege" component={Historyofcollege} />
-                <Route exact path="/principalmessage" component={Principal} />
-                <Route exact path="/chairmanmessage" component={Chairman} />
-                <Route exact path="/admin" component={Admin} />
-                <Route exact path="/student" component={Student} />
-                <Route exact path="/staff" component={Staff} />
-                <Route exact path="/forgotpasswordhandler/:id/:hash" component={Forgotpassword} />
-                {/* <Route path='/404' component={My404Component} /> */}
-                <Redirect from='*' to='/' />
-            </Switch>
-        </div>
+            <div className="core-container">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/infrastructure" component={Infrastructure} />
+                    <Route exact path="/gallery" component={Gallery} />
+                    <Route exact path="/alumni" component={Alumni} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route exact path="/mech" component={Mech} />
+                    <Route exact path="/eee" component={Eee} />
+                    <Route exact path="/ece" component={Ece} />
+                    <Route exact path="/cse" component={Cse} />
+                    <Route exact path="/hands" component={Hands} />
+                    <Route exact path="/mecse" component={Mecse} />
+                    <Route exact path="/engdesign" component={Engdesign} />
+                    <Route exact path="/mba" component={Mba} />
+                    <Route exact path="/historyofcollege" component={Historyofcollege} />
+                    <Route exact path="/principalmessage" component={Principal} />
+                    <Route exact path="/chairmanmessage" component={Chairman} />
+                    <Route exact path="/admin" component={Admin} />
+                    <Route exact path="/student" component={Student} />
+                    <Route exact path="/staff" component={Staff} />
+                    <Route exact path="/forgotpasswordhandler/:id/:hash" component={Forgotpassword} />
+                    {/* <Route path='/404' component={My404Component} /> */}
+                    <Redirect from='*' to='/' />
+                </Switch>
+            </div>
+        </>
     </BrowserRouter>, document.getElementById('root'));
